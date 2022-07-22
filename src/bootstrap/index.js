31a6e502994761296ab9bc571c033be60ed49908
Instabot <instabot@example.com>
2022-07-22 09:12:04 +0100

    DHT moved to own repo
    Can now be found in https://github.com/synonymdev/slashtags-bootstrap-server

--- src/bootstrap/index.js
+++
@@ -1,7 +0,0 @@
-import config from 'config'
-import DHT from '@hyperswarm/dht'
-
-const port = config.get('bootstrapper.port')
-const node = new DHT({ port })
-
-console.log(`Bootstrapping DHT node started on port ${port}`)
